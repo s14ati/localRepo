404308b10b148cb533ed8237a00ed01d0493623d
s14ati <swati14012000@gmail.com>
2025-07-16 15:11:55 +0530

    add initial files

---
+++ Local.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Local() {
+  return (
+    <div>
+        <p>This is new button</p>
+        <button>CLICK ME</button>
+    </div>
+  )
+}
+
+export default Local
